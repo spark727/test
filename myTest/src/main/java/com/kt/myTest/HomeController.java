db75fe54ea6627737c2c0f802ff2120db43cde09
spark727 <park27@gmail.com>
2016-04-29 11:39:06 +0900

    Update HomeController.java

--- myTest/src/main/java/com/kt/myTest/HomeController.java
+++ myTest/src/main/java/com/kt/myTest/HomeController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
+	// edit something
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
